e03091da7d26a6002dd689adb082bf8077e7822b
LeeNoel <112954324+LeeNoel25@users.noreply.github.com>
2023-02-22 16:57:24 +0800

    WIP

--- W3wordle.js
+++ W3wordle.js
@@ -101,12 +101,16 @@ const KEYS = [
     { char: "BACKSPACE" },
   ],
 ];
+let SCREEN = "intro";
 /*----- cached elements  -----*/
+//
+const playbutton = document.querySelector("#playButton");
+
 const introScreen = document.querySelector("#intro");
 const gameScreen = document.querySelector("#game");
 const statScreen = document.querySelector("#stats");
 const screenList = [introScreen, gameScreen, statScreen];
-
+//
 const gameBoard = document.querySelector("#gameBoard");
 const keyBoard = document.querySelector("#keyBoard");
 const replayButton = document.querySelector("#playAgainButton");
@@ -117,12 +121,23 @@ let playerGuess = [];
 //
 let guessesRemaining = CHANCES;
 let cellIndex = 0;
+//
+// let gameBoardCell = [];
 
 /*----- functions -----*/
-function toGameScreen() {
-  game.screen = "game";
+
+function resetGameBoard() {
+  gameBoard.innerHTML = "";
+  keyBoard.innerHTML = "";
+  gameWord(GAME_WIDTH);
+  gameAnswer = gameWord(GAME_WIDTH);
+  console.log(gameAnswer);
+  // let gameAnswer = gameWord(GAME_WIDTH);
+  // console.log(gameAnswer);
+  renderGameBoard();
+  renderKeyBoard();
 }
-function selectWord(wordLength) {
+function gameWord(wordLength) {
   let filteredWords = [];
   // Filter words based on desired length
   for (let i = 0; i < WORDLIST.length; i++) {
@@ -134,29 +149,40 @@ function selectWord(wordLength) {
   const randomIndex = Math.floor(Math.random() * filteredWords.length);
   return filteredWords[randomIndex];
 }
+gameWord(GAME_WIDTH);
+let gameAnswer = gameWord(GAME_WIDTH);
+console.log(gameAnswer);
+
 function renderScreen() {
   screenList.forEach((ele) => ele.classList.add("hidden"));
-  const showScreen = document.querySelector("#" + game.screen);
+  const showScreen = document.querySelector("#" + SCREEN);
   showScreen.classList.remove("hidden");
 }
+function toGameScreen() {
+  SCREEN = "game";
+}
+
+function playGame() {
+  resetGameBoard();
+}
+//----------------------------------------
 function renderGameBoard() {
-  console.log("test");
+  // gameBoardCell = []
   for (let i = 0; i < CHANCES; i++) {
+    // gameBoardCell[i] = [];
     const boardRow = document.createElement("div");
     boardRow.classList.add("boardRow");
     for (let j = 0; j < GAME_WIDTH; j++) {
       const boardCell = document.createElement("span");
-      boardCell.innerText = "";
+      boardCell.innerText = ""; // maybe not needed
       boardCell.classList.add("boardCell");
       boardRow.appendChild(boardCell);
     }
     gameBoard.appendChild(boardRow);
   }
 }
-renderGameBoard();
 
 function renderKeyBoard() {
-  console.log("dog");
   for (let i = 0; i < KEYS.length; i++) {
     const keyRow = document.createElement("div");
     keyRow.classList.add("keyRow");
@@ -166,9 +192,10 @@ function renderKeyBoard() {
       keyCell.classList.add("keyCell"); // cursor: pointer;
       /////////
       keyCell.addEventListener("click", (key) => {
-        let clickedChar = key.char;
+        let clickedChar = key.target.innerText;
+        console.log(clickedChar);
         if (clickedChar === "ENTER") {
-          checkPlayerGuess();
+          checkPlayerGuess(playerGuess);
           return;
         } else if (clickedChar === "BACKSPACE" && cellIndex !== 0) {
           deleteLetter();
@@ -177,20 +204,6 @@ function renderKeyBoard() {
           inputKeys(clickedChar);
         }
       });
-      //       if (KEYS[i][j].char === "ENTER") {
-      //         keyCell.addEventListener("click", () => {
-      //           checkPlayerGuess();
-      //           // return;
-      //         });
-      //       } else if (KEYS[i][j].char === "BACKSPACE" && cellIndex !== 0) {
-      //         keyCell.addEventListener("click", () => {
-      //           deleteLetter();
-      //           // return;
-      //         });
-      //       } else {
-      //         keyCell.addEventListener("click", () => {
-      //           inputKeys(KEYS[i][j]);
-      //         });
       keyRow.appendChild(keyCell);
     }
     //
@@ -198,44 +211,98 @@ function renderKeyBoard() {
   }
 }
 
-renderKeyBoard();
-
 function inputKeys(clickedKey) {
-  console.log(cellIndex); // 0
-  // Max chances reached
-  if (guessesRemaining === 0) {
-    return;
-  }
+  // console.log(cellIndex); // 0
   // Max chances reached
   if (cellIndex === GAME_WIDTH + 1) {
     return;
   }
   // Letters
-  let inputChar = clickedKey ? clickedKey.char : ""; // chatGPT: added a check for clickedKey to make sure it is defined before trying to access its char property. If clickedKey is undefined, we set inputChar to an empty string.
-  let row = document.querySelectorAll(".boardRow")[CHANCES - guessesRemaining];
-  let cell = row.children[cellIndex]; // free code camp - try gameBoard[i][j]
-  cell.innerText = inputChar;
+  // identifying the present cell
+  let row = document.querySelectorAll(".boardRow")[CHANCES - guessesRemaining]; // 0
+  let cell = row.children[cellIndex]; // free code camp - try gameBoard[i][j] // 0
+  cell.innerText = clickedKey;
   cell.classList.add("filled-cell"); // free code camp
-  playerGuess.push(inputChar); // ["C", ...]
+  playerGuess.push(clickedKey); // ["C", ...]
   cellIndex++;
 }
-
-inputKeys();
 
 function deleteLetter() {
   // gets the correct row, finds the last box and empties it, and then resets the nextLetter counter
   let row = document.querySelectorAll(".boardRow")[CHANCES - guessesRemaining];
-  let cell = row.children[cellIndex - 1];
+  let cell = row.children[cellIndex - 1]; // children
   cell.innerText = "";
   cell.classList.remove("filled-cell");
-  playerGuess.pop;
+  playerGuess.pop();
   cellIndex--;
+  // if ((cell.innerText = "")) {
+  //   return;
+  // }
 }
 
-function checkPlayerGuess() {
-  console.log("test");
+function checkPlayerGuess(playerGuess) {
+  let row = document.querySelectorAll(".boardRow")[CHANCES - guessesRemaining];
+  let key = document.querySelectorAll(".keyCell");
+  console.log(playerGuess);
+  console.log(gameAnswer);
+  console.log(row[i].innerText);
+  if (playerGuess.length != GAME_WIDTH) {
+    alert("Not enough letters!");
+    return;
+  }
+  // ---------------------------------
+  for (let i = 0; i < GAME_WIDTH; i++) {
+    if (row[i].innerText === gameAnswer[i]) {
+      //-----------
+      row[i].style.backgroundColor = "green";
+      key[i].style.backgroundColor = "green";
+      alert("Well Done!");
+      return;
+      //---------------------
+    } else if (gameAnswer.includes(playerGuess[i])) {
+      row[i].style.backgroundColor = "yellow";
+      key[i].style.backgroundColor = "yellow";
+    } else {
+      row[i].style.backgroundColor = "red";
+      key[i].style.backgroundColor = "red";
+    }
+  }
 }
 
-function changeCellColor() {
-  console.log("test");
+// function changeCellColor() {
+//   console.log("test");
+// }
+
+// Put difficulty options - add reset the game board
+function main() {
+  playbutton.addEventListener("click", playGame);
+
+  const fourLetters = document.querySelector("#fourLetters");
+  fourLetters.addEventListener("click", () => {
+    GAME_WIDTH = 4;
+    console.log(GAME_WIDTH);
+    resetGameBoard();
+  });
+
+  const fiveLetters = document.querySelector("#fiveLetters");
+  fiveLetters.addEventListener("click", () => {
+    GAME_WIDTH = 5;
+    console.log(GAME_WIDTH);
+    resetGameBoard();
+  });
+
+  const sixLetters = document.querySelector("#sixLetters");
+  sixLetters.addEventListener("click", () => {
+    GAME_WIDTH = 6;
+    console.log(GAME_WIDTH);
+    resetGameBoard();
+  });
 }
+main();
+
+// Swiitch between screens
+
+// win lose GIF - MODAL
+// music
+
+// Track the win streak - RESET THE GAME BAORD
